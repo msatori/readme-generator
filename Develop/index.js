10cdc06e23b2979a2cc47898e95a5cda2ce4f921
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-28 15:31:31 -0500

    created basic outline of script

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,21 @@
+//install inquirer and fs
+const inquirer = require('inquirer');
+const fs = require('fs');
 // array of questions for user
 const questions = [
 
 ];
-
+//use inquirer to prompt questions for readme requirements
+inquirer
+.prompt {[
+    {}
+]}
 // function to write README file
 function writeToFile(fileName, data) {
+    //create constant to generate file name 
+    const fileName = data.name.toLowerCase().split('').join('') + 'json';
+
+
 }
 
 // function to initialize program
