fa91dd874963b7f0dc97daf2ae4056e1c472af2f
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-30 16:51:02 -0500

    created readme outline

---
+++ Develop/README.md
@@ -0,0 +1,1 @@
+My github username is satorim24
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+    ## Description 
+
+    ${data.description}
+
+
+    ## Table of Contents (Optional)
+
+      * [Installation](#installation)
+      * [Usage](#usage)
+      * [Credits](#credits)
+      * [License](#license)
+
+
+    ## Installation
+
+
+    ## Usage 
+
+
+    ## Credits
+
+
+    ## License
+
+
+    ## Badges
+
+    ## Features
+
+    ## Contributing
+
+    ## Tests
+
 `;
 }
 
